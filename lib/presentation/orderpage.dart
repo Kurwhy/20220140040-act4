b82d648b2e4fd9846697e6154c43dbfe403311de
Wahyu <firmansyahw503@gmail.com>
2025-04-23 19:00:16 +0700

    controller untuk input dan fungsi hitung total harga

---
+++ lib/presentation/orderpage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:play_navigation/presentation/orderdetailpage.dart';
+
+class OrderPage extends StatefulWidget {
+  const OrderPage({super.key});
+
+  @override
+  State<OrderPage> createState() => _OrderPageState();
+}
+
+class _OrderPageState extends State<OrderPage> {
+  final TextEditingController makananController = TextEditingController();
+  final TextEditingController minumanController = TextEditingController();
+  final TextEditingController jumlahMakananController = TextEditingController();
+  final TextEditingController jumlahMinumanController = TextEditingController();
+  int totalHarga = 0;
+
+  void calculateTotalPrice() {
+    int jumlahMakanan = int.tryParse(jumlahMakananController.text) ?? 0;
+    int jumlahMinuman = int.tryParse(jumlahMinumanController.text) ?? 0;
+
+    setState(() {
+      totalHarga = (jumlahMakanan * 32000) + (jumlahMinuman * 5000);
+    });
+  }
+
+  
+}

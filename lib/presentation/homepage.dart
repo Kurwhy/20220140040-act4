6c7339af3b36cec192b156408929bb15dd43b2f0
Wahyu <firmansyahw503@gmail.com>
2025-04-23 18:56:03 +0700

    AppBar dan tombol navigasi ke OrderPage di Homepage

--- lib/presentation/homepage.dart
+++ lib/presentation/homepage.dart
@@ -12,7 +12,22 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(title: Text('Home Page')),
+      body: Center(
+        child: Column(
+          spacing: 8,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Selamat datang di Home Page!'),
+            ElevatedButton(onPressed: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const OrderPage()),
+              );
+            }, child: Text('Order Now')),
+          ],
+        ),
+      ),
     );
   }
 }

03ee12239004e2f2558be589ed138c4dfba12d99
Wahyu <firmansyahw503@gmail.com>
2025-04-23 18:39:42 +0700

    struktur awal LoginPage dengan controller dan scaffold

---
+++ lib/presentation/loginpage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:play_navigation/presentation/homepage.dart';
+import 'package:play_navigation/presentation/register.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+  
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold(
+      
+    );
+  }
+}
